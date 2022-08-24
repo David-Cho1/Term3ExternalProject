31eabbb637733526b5fc821257f22b2ee347f279
Jihoon Choi <choij2821@gmail.com>
2022-08-24 20:44:00 +1200

    8/24/2022 Menu

--- main.py
+++ main.py
@@ -1,49 +1,113 @@
 from tkinter import *
 
-#menu
+# Menu Window
+
 Menu = Tk()
 Menu.title("Paper Scissors Rock")
-Menu.configure(width=1920, height=1080)
+Menu.attributes('-fullscreen', True)
+Menu['bg'] = '#333d79'
+
+#alpha, -transparentcolor, -disabled, -fullscreen, -toolwindow, or -topmost
+
+# Big Title of the game picture in the menu window
 
 
 
+# Multiplayer Window Open
+def multiplayer_win():
 
-#Multiplayer Button
+    Menu.destroy()          # Close Menu Window
+
+    # Makes Multiplayer game window
+    Multiplayer_win = Tk()
+    Multiplayer_win.title("Paper Scissors Rock")
+    Multiplayer_win['bg'] = '#333d79'
+    Multiplayer_win.attributes('-fullscreen', True)
+
+    def back():
+        Multiplayer_win.destroy()
+
+    #Back to menu button
+    mp_back_btn = Button(Multiplayer_win, text="Back", command=back)
 
 
-multiplayer_game = Tk()
-multiplayer_game.title("Paper Scissors Rock")
 
+# Multiplayer Button
+multiplayer_btn = Button(Menu, text="Multiplayer", command=multiplayer_win)
+multiplayer_btn['bg'] = 'yellow'      #faebef
+multiplayer_btn['fg'] = '#333d79'
+multiplayer_btn['bd'] = 15
 
-multiplayer = Button(Menu, text="Multiplayer")
-multiplayer.place(x=1500, y=400,width=200,height=100)
-if multiplayer == "Multiplayer":
-    multiplayer_game = Tk()
-    multiplayer_game.title("Paper Scissors Rock")
-    multiplayer_game.configure(width=1920, height=1080)
+multiplayer_btn.config(font=("Alumni Sans Collegiate One", 30, "italic", "bold"))
+multiplayer_btn.place(relx=0.6, rely=0.1, width=500, height=100)
 
+# function that changes the colour of the Multiplayer button when mouse is on it
+def multi_button_color_ent(e):
+    multiplayer_btn['bg'] = '#FFCD00'
 
+def multi_button_color_lea(e):
+    multiplayer_btn['bg'] = "yellow"
+
+# When Mouse Courser is on Multiplayer button
+multiplayer_btn.bind("<Enter>", multi_button_color_ent)
+multiplayer_btn.bind("<Leave>", multi_button_color_lea)
 
 
 
 #Bot Button
 def Bot():
-    print("LOL Too obvious")
+    Menu.destroy()          # Close Menu Window
+    # Makes Bot game window
+    bot_game = Tk()
+    bot_game.title("Paper Scissors Rock")
+    bot_game['bg'] = '#333d79'
+    bot_game.attributes('-fullscreen', True)
+
+# Bot Button
+bot_btn = Button(Menu, text="Play Agianst Bot", command=Bot)
+bot_btn['bg'] = 'yellow'      #faebef
+bot_btn['fg'] = '#333d79'
+bot_btn['bd'] = 15
+bot_btn.config(font=("Alumni Sans Collegiate One", 30, "italic", "bold"))
+bot_btn.place(relx=0.6, rely=0.4, width=500, height=100)
+
+# function that changes the colour of the Bot button when mouse is on it
+def bot_button_color_ent(e):
+    bot_btn['bg'] = '#FFCD00'
+
+def bot_button_color_lea(e):
+    bot_btn['bg'] = "yellow"
+
+# When Mouse Courser is on Bot button
+bot_btn.bind("<Enter>", bot_button_color_ent)
+bot_btn.bind("<Leave>", bot_button_color_lea)
 
 
 
-Bot = Button(Menu, text="Bot", command=Bot)
-Bot.place(x=450, y=400,width=70,height=40)
-
-
-
-
-#Exit the game
+# Exit the game
 def Exit():
     Menu.quit()
 
-Exit = Button(Menu, text="Exit", command=Exit)
-Exit.place(x=50, y=100,width=70,height=40)
+# Exit Button
+exit_btn = Button(Menu, text="Exit", command=Exit)
+exit_btn['bg'] = 'red'      #faebef
+exit_btn['fg'] = '#333d79'
+exit_btn['bd'] = 15
+exit_btn.config(font=("Alumni Sans Collegiate One", 30, "italic", "bold"))
+exit_btn.place(relx=0.6, rely=0.7, width=500, height=100)
+
+# function that changes the colour of the Exit button when mouse is on it
+def exit_button_color_ent(e):
+    exit_btn['bg'] = '#D90000'
+
+def exit_button_color_lea(e):
+    exit_btn['bg'] = "red"
+
+# When Mouse Courser is on Exit button
+exit_btn.bind("<Enter>", exit_button_color_ent)
+exit_btn.bind("<Leave>", exit_button_color_lea)
+
+
 
 
 
